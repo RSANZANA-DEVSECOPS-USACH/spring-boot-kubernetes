c31af8dbd1b4af9e11063c3bb3e6cdd5d3d84799
rsanzana <rsanzana@previred.com>
2022-10-30 00:25:23 -0300

    pipeline final

--- src/main/java/org/soyphea/k8s/vulnerable.java
+++ src/main/java/org/soyphea/k8s/vulnerable.java
@@ -1,11 +1,20 @@
-Intent in = getIntent();
-String path = in.getStringExtra("path");
-if(path == null)
-return;
-String sdcard =  Environment.getExternalStorageDirectory()
-if(path.startsWith(sdcard))
-{
-	Log.e(TAG, "Attempt to write to sdcard");
-	return;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipEntry;
+⋮
+public void extract(ZipFile zip) {
+     ⋮
+     String toDir = "/my/target/directory/";
+     Enumeration entries = zip.entries();
+     while (entries.hasMoreElements()) {
+        ZipEntry zipEntry = entries.nextElement();
+        ⋮
+        File file = new File(toDir, zipEntry.getName())
+        if( !file.getCanonicalPath().startsWith(toDir) ){
+          throw new SecurityException("ZipEntry not within target directory!");
+        }
+        InputStream istr = zipFile.getInputStream(zipEntry);
+        final OutputStream os = Files.newOutputStream(file.toPath());
+        bos  = new BufferedOutputStream(os);
+        IOUtils.copy(bis, bos);
+    }
 }
-writeToFile(path);
